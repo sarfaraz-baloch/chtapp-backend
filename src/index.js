f4263e3f7d74f3dc69a823d832d63a4335daab94
sarfaraz-baloch <sarfarazkohda321@gmail.com>
2024-11-23 02:54:48 +0500

    working to fix bug

--- src/index.js
+++ src/index.js
@@ -1,55 +1,99 @@
-// 
+// // 
 
-import express from "express"; // import express
-import dotenv from "dotenv"; // import dotenv for environment variables
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
+// import express from "express"; // import express
+// import dotenv from "dotenv"; // import dotenv for environment variables
+// import authRoutes from "./routes/auth.route.js";
+// import messageRoutes from "./routes/message.route.js";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import { connectDb } from "./lib/db.js";
+// import { app , server} from "./lib/socket.js";
+// import path from "path";
+// import { get } from "http";
+
+
+// dotenv.config();
+
+// // Set the payload limit to 50MB
+// app.use(express.json({ limit: '50mb' })); // This will increase the limit for JSON bodies
+// app.use(cookieParser());
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   // origin: "*",
+//   credentials: true
+// }));
+
+// const PORT = process.env.PORT || 5001;
+// const __dirname = path.resolve();
+
+
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend", "build", "/index.html"));
+//   });
+// }
+
+// console.log("NODE_ENV:", process.env.NODE_ENV);
+
+
+// server.listen(PORT, () => {
+//   console.log("Server is running on port " + PORT);
+//   connectDb();
+// });
+
+// // app.get('/', (req, res) => {
+// //   res.send('Hello, World!');
+// // });
+// // server.listen(PORT, '0.0.0.0', () => {
+// //   console.log(`Server is running on port ${PORT}`);
+// // });
+
+
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { connectDb } from "./lib/db.js";
-import { app , server} from "./lib/socket.js";
-import path from "path";
-import { get } from "http";
 
+import path from "path";
+
+import { connectDb } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-// Set the payload limit to 50MB
-app.use(express.json({ limit: '50mb' })); // This will increase the limit for JSON bodies
-app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  // origin: "*",
-  credentials: true
-}));
-
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-console.log("NODE_ENV:", process.env.NODE_ENV);
-
-
 server.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDb();
 });
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, World!');
-// });
-// server.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
